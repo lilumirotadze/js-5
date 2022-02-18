b0943c9764fa158cfc970b32b1d8579571f5cc4b
lilu mirotadze <91903045+lilumirotadze@users.noreply.github.com>
2022-02-18 19:22:35 +0400

    not full

---
+++ script.js
@@ -0,0 +1,80 @@
+// let array = [5, 25, 89, 120, 36];
+// array.push('javascript','python');
+// array.unshift('html, "css');
+// let lenght = array.length;
+// console.log(lenght);
+// array.shift();
+// array.pop();
+
+// console.log(array);
+
+
+// number 2
+
+// let fruits = ['Orange','banana','pears'];
+// let length = fruits.length;
+// console.log(length);
+// fruits.push('apple', 'ananas');
+// fruits.unshift('Watermelon');
+// let.length = fruits.length;
+// let newArrayFruits = fruits.slice(1,2);
+// console.log(newArrayFruits);
+// let length = fruits.length;
+// console.log(length);
+
+// console.log(fruits);
+
+
+
+// number 3
+
+// let numbers = [12, 25, 3, 6, 8, 14, 7, 23];
+// let newArrayNumbers = numbers.map(function(item){
+    // return item / 3;
+// });
+
+// console.log(newArrayNumbers);
+
+// number 4
+
+// let masive = ['hello', 125, 'javascript', 'html', 43, 'css', 'scss', 'bootstrap', 88, 59, 'python'];
+// let newArrayMasive = masive.filter(function(item){
+    // return typeof item == 'string';
+// });
+// console.log(newArrayMasive);
+// 
+
+// number 5
+
+// let languages = ['html','css', 'javascript','python','php'];
+// let result = languages.filter(function(item){
+// return item.length > 3 });
+
+// console.log(result);
+
+
+// number 6
+
+let array= ['academy', 'of', 'digital', 'industries'].reduce(function(x,y){
+    return x.concat(y)
+},
+[]
+);
+
+console.log(array);
+
+
+
+
+// number 7
+// 
+
+
+
+// number 8
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+// let result = words.filter(function(item){
+// return item.includes('m', 'M')
+// });
+
+// console.log(result);
